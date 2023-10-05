0fedbc4eeb34a5001fa35ec04e2111e6f9371d1c
Dilip Roshitha <dilip.roshitha@grubtech.com>
2023-10-05 23:36:11 +0530

    [NT] redesign browser plugin. added chrome support

--- Extension/src/service/tradingViewTradeInfo.service.ts
+++ Extension/src/service/tradingViewTradeInfo.service.ts
@@ -1,14 +1,20 @@
 import { TradingViewTradeInfo } from "../models/commonModels";
+import { BrowserType, getBrowserType } from "./browserUtil.service";
 import { isDevMode } from "./configuration.service";
 import { getTradingViewTradeInfoTestData } from "./testData.service";
 
-const getTradeInfoFirefox = async (): Promise<TradingViewTradeInfo[]>  =>  {
-    let result: TradingViewTradeInfo[] = [];
+export const getTradeInfo = async (): Promise<TradingViewTradeInfo | null>  =>  {
+    let result: TradingViewTradeInfo | null = null;
     try {
-        result = await browser.tabs.executeScript({ file: "/content_scripts/fetchTradeInfo.js" });
+        const browserType: BrowserType = getBrowserType();
+        if (browserType == BrowserType.FIREFOX) {
+            result = await getTradeFirefox();
+        } else {
+            result = await getTradeChrome();
+        }
     } catch (err) {
         if (isDevMode()) {
-            result = [getTradingViewTradeInfoTestData()];
+            result = getTradingViewTradeInfoTestData();
         } else {
             console.error(err);
         }
@@ -17,5 +23,19 @@ const getTradeInfoFirefox = async (): Promise<TradingViewTradeInfo[]>  =>  {
     return result;
 };
 
+const getTradeChrome = async (): Promise<TradingViewTradeInfo | null> => {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+    const injectionResult = await chrome.scripting.executeScript({ target: { tabId: tab.id || 0 }, files: ["/content_scripts/fetchTradeInfo.js"] });
+    if (injectionResult && injectionResult.length > 0) {
+        return injectionResult[0].result as TradingViewTradeInfo;
+    }
+    return null;
+}
 
-export {getTradeInfoFirefox};
+const getTradeFirefox = async (): Promise<TradingViewTradeInfo | null> => {
+    const result = await browser.tabs.executeScript({ file: "/content_scripts/fetchTradeInfo.js" });
+    if (result && result.length > 0) {
+        return result[0];
+    }
+    return null;
+}
